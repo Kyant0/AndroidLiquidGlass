c44447c83dadb77933aad59b3d9c0bbbdcee82b7
Kyant0 <kyant2021@outlook.com>
2025-10-11 21:37:40 +0800

    Remove `compose-compiler-report-generator` plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,4 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("dev.shreyaspatil.compose-compiler-report-generator") version "1.4.2" apply false
 }
