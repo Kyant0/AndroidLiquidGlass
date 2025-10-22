58871de4aad8ccedb49a2c0163644a64b51e340b
Kyant0 <kyant2021@outlook.com>
2025-10-22 20:59:04 +0800

    Update progressive blur

--- catalog/src/main/java/com/kyant/backdrop/catalog/destinations/ProgressiveBlurContent.kt
+++ catalog/src/main/java/com/kyant/backdrop/catalog/destinations/ProgressiveBlurContent.kt
@@ -1,7 +1,6 @@
 package com.kyant.backdrop.catalog.destinations
 
 import android.graphics.RenderEffect
-import android.graphics.RuntimeShader
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
@@ -45,21 +44,24 @@ fun ProgressiveBlurContent() {
                             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                                 effect(
                                     RenderEffect.createRuntimeShaderEffect(
-                                        RuntimeShader(
+                                        obtainRuntimeShader(
+                                            "AlphaMask",
                                             """
 uniform shader content;
 
 uniform float2 size;
 layout(color) uniform half4 tint;
+uniform float tintIntensity;
 
 half4 main(float2 coord) {
     float blurAlpha = smoothstep(size.y, size.y * 0.5, coord.y);
     float tintAlpha = smoothstep(size.y, size.y * 0.5, coord.y);
-    return mix(content.eval(coord) * blurAlpha, tint * tintAlpha, 0.5);
+    return mix(content.eval(coord) * blurAlpha, tint * tintAlpha, tintIntensity);
 }"""
                                         ).apply {
                                             setFloatUniform("size", size.width, size.height)
                                             setColorUniform("tint", tintColor.toArgb())
+                                            setFloatUniform("tintIntensity", 0.8f)
                                         },
                                         "content"
                                     )
